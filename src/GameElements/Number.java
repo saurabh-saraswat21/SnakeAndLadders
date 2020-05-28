10f5a77ce96f84f2a516ad96700b28ad293efbe7
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-05-28 11:48:59 +0530

    add getter and setter

--- src/GameElements/Number.java
+++ src/GameElements/Number.java
@@ -4,5 +4,20 @@ public class Number {
     private boolean isLadder;
     private  boolean isSnake;
 
-    
+
+    public int getValue() {
+        return value;
+    }
+
+    public boolean isLadder() {
+        return isLadder;
+    }
+
+    public boolean isSnake() {
+        return isSnake;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
