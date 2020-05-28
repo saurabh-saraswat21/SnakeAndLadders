c0903f9a4669569e438ba8e23a6169638e215a2f
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-05-28 12:29:52 +0530

    add getter and setter

--- src/GameElements/Snake.java
+++ src/GameElements/Snake.java
@@ -3,4 +3,19 @@ package GameElements;
 public class Snake {
     private Number head ;
     private Number tail;
+    public void setHead(Number head) {
+        this.head = head;
+    }
+
+    public void setTail(Number tail) {
+        this.tail = tail;
+    }
+
+    public Number getHead() {
+        return head;
+    }
+
+    public Number getTail() {
+        return tail;
+    }
 }
