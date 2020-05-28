61582901596bb40a790a078643a67af40a2aad9a
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-05-28 12:00:11 +0530

    add getter and setter

--- src/GameElements/Ladder.java
+++ src/GameElements/Ladder.java
@@ -3,6 +3,20 @@ package GameElements;
 public class Ladder {
     private Number Head ;
     private Number Tail;
+    public void setHead(Number head) {
+        Head = head;
+    }
 
+    public void setTail(Number tail) {
+        Tail = tail;
+    }
+
+    public Number getHead() {
+        return Head;
+    }
+
+    public Number getTail() {
+        return Tail;
+    }
 
 }
