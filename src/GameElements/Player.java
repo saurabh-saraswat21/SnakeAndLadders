aa3b9209f12ab72ba8493747150f950c6707e62c
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-05-28 12:20:56 +0530

    add getter and setter

--- src/GameElements/Player.java
+++ src/GameElements/Player.java
@@ -1,7 +1,23 @@
 package GameElements;
 
 public class Player {
-private Number currentPosition;
-private boolean isLock;
+    private Number currentPosition;
+    private boolean isLock;
+
+    public boolean isLock() {
+        return isLock;
+    }
+
+    public void setLock(boolean lock) {
+        isLock = lock;
+    }
+
+    public Number getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public void setCurrentPosition(Number currentPosition) {
+        this.currentPosition = currentPosition;
+    }
 
 }
