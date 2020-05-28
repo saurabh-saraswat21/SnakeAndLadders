8375bc2dea503ca9ccc7bd1fa4063698048ec21b
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-05-28 10:39:10 +0530

    add roll Dice Method

--- src/GameElements/Dice.java
+++ src/GameElements/Dice.java
@@ -1,4 +1,10 @@
 package GameElements;
 
+import java.util.Random;
 public class Dice {
+    public int rollDice() {
+        Random random = new Random();
+        int number = random.nextInt(6);
+        return number +1;
+    }
 }
