710408d745d8c59a56902efc3c0dd5895b05f23f
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-05-29 12:36:10 +0530

    Add getter of board variable

--- src/GameElements/Board.java
+++ src/GameElements/Board.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 public class Board {
     private static final ArrayList<Number> board = new ArrayList<>();
 
+    public static ArrayList<Number> getBoard() {
+        return board;
+    }
+
     private static void loadBoard() {
 
         for (int i = 1; i <= 100; i++) {
