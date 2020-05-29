fbae053688ddcbcf1ee95037468f0fa9d8a6afa5
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-05-29 10:51:28 +0530

    fix loadBoard method

--- src/GameElements/Board.java
+++ src/GameElements/Board.java
@@ -3,13 +3,13 @@ package GameElements;
 import java.util.ArrayList;
 
 public class Board {
-    private static  ArrayList<Number> board = new ArrayList<>();
-    private static  void LoadBoard()
-    {
-        Number n1 = new Number();
-        for (int i =1 ; i <= 100; i++) {
-            n1.setValue(i);
-            board.add(n1);
+    private static final ArrayList<Number> board = new ArrayList<>();
+
+    private static void loadBoard() {
+
+        for (int i = 1; i <= 100; i++) {
+            board.add(new Number(i));
+
         }
     }
 
