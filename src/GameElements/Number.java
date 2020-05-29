dd014e1ff90676e7865bd60a7427fbf223339b20
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-05-29 11:34:58 +0530

    remove the constructor with only value variable

--- src/GameElements/Number.java
+++ src/GameElements/Number.java
@@ -58,10 +58,6 @@ public class Number {
         return Integer.compare(number.getValue(), integer);
     }
 
-    public Number(int value) {
-        this.value = value;
-    }
-
     @Override
     public String toString() {
         return String.valueOf(this.getValue()) + " ";
