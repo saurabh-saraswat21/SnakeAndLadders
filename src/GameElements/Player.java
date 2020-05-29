1e68ddba0d6aff4a403cb486db46c9877ec3c0ab
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-05-29 12:26:26 +0530

    add getter and setter of name variable

--- src/GameElements/Player.java
+++ src/GameElements/Player.java
@@ -11,6 +11,14 @@ public class Player {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public boolean isLock() {
         return isLock;
     }
