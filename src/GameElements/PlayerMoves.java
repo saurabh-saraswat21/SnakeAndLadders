cfc93a35c3243e09f00fbaf5490a497511de33d8
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-05-29 17:37:37 +0530

    Add a private method that takes a Player and a dice output and move the player accordingly

--- src/GameElements/PlayerMoves.java
+++ src/GameElements/PlayerMoves.java
@@ -1,5 +1,10 @@
 package GameElements;
 
 public class PlayerMoves {
+    private void move(Player player, int number) {
+        Number n1 = Board.getBoard().get(number - 1);
+        player.setCurrentPosition(n1);
+    }
+
 
 }
