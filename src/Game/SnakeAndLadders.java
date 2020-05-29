304497219d399b98641cad1760d04a7911add9e9
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-05-29 17:12:21 +0530

    add a method getPlayers that getNumber of Players and their details and return the ArrayList consists of the players

--- src/Game/SnakeAndLadders.java
+++ src/Game/SnakeAndLadders.java
@@ -1,5 +1,27 @@
 package Game;
 
+import GameElements.Board;
+import GameElements.Player;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class SnakeAndLadders {
+    private static ArrayList<Player> getPlayers() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Number Of Players:-");
+        int noOfPlayers = sc.nextInt();
+        String name;
+        Player player;
+        ArrayList<Player> players = new ArrayList<>();
+        for (int i = 0; i < noOfPlayers; i++) {
+            System.out.println("Enter" + (i + 1) + "Player Name");
+            name = sc.next();
+            player = new Player(Board.getBoard().get(0), true, name);
+            players.add(player);
+        }
+        sc.close();
+        return players;
+    }
 
 }
