93667f86ddd6462db111ecef2432ab9dcbd1228a
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-05-29 09:28:22 +0530

    getter and setter of variables

--- src/GameElements/Number.java
+++ src/GameElements/Number.java
@@ -2,11 +2,37 @@ package GameElements;
 public class Number {
     private int value;
     private boolean isLadder;
-    private  boolean isSnake;
+    private boolean isSnake;
+    private Ladder ladder;
+    private Snake snake;
 
+    public Ladder getLadder() {
+        return ladder;
+    }
+
+    public void setLadder(Ladder ladder) {
+        this.ladder = ladder;
+    }
+
+    public void setLadder(boolean ladder) {
+
+        isLadder = ladder;
+    }
+
+    public Snake getSnake() {
+        return snake;
+    }
 
     public int getValue() {
         return value;
+    }
+
+    public void setSnake(Snake snake) {
+        this.snake = snake;
+    }
+
+    public void setSnake(boolean snake) {
+        isSnake = snake;
     }
 
     public boolean isLadder() {
