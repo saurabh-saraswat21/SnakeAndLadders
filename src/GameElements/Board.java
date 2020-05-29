4a357df894f671e7ccbe9315cdfbab8d24644247
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-05-29 09:08:25 +0530

    add loadBoard method

--- src/GameElements/Board.java
+++ src/GameElements/Board.java
@@ -1,4 +1,16 @@
 package GameElements;
 
+import java.util.ArrayList;
+
 public class Board {
+    private static  ArrayList<Number> board = new ArrayList<>();
+    private static  void LoadBoard()
+    {
+        Number n1 = new Number();
+        for (int i =1 ; i <= 100; i++) {
+            n1.setValue(i);
+            board.add(n1);
+        }
+    }
+
 }
