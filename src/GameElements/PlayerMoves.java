bfeec2a6deb66d462f50f3d33397aa508b8716e0
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-06-01 11:03:11 +0530

    make All methods of PlayerMoves.java class static

--- src/GameElements/PlayerMoves.java
+++ src/GameElements/PlayerMoves.java
@@ -1,42 +1,42 @@
 package GameElements;
 
 public class PlayerMoves {
-    public boolean checkWin(Player p1) {
+    public static boolean checkWin(Player p1) {
         return p1.getCurrentPosition().getValue() == 100;
     }
 
-    private int moveAndCheck(Player p1, int number) {
+    private static int moveAndCheck(Player p1, int number) {
         int response = 0;
-        this.justMove(p1, number);
-        boolean ladder = this.checkLadder(p1);
+        justMove(p1, number);
+        boolean ladder = checkLadder(p1);
         if (!ladder) {
-            if (this.checkSnake(p1))
+            if (checkSnake(p1))
                 response = -1;
         } else
             response = 1;
         return response;
     }
 
-    private void riseLadder(Player p1) {
+    private static void riseLadder(Player p1) {
         Ladder ladder = p1.getCurrentPosition().getLadder();
         p1.setCurrentPosition(ladder.getHead());
     }
 
-    private void byteSnake(Player p1) {
+    private static void byteSnake(Player p1) {
         Snake snake = p1.getCurrentPosition().getSnake();
         p1.setCurrentPosition(snake.getTail());
     }
 
-    private void justMove(Player player, int number) {
+    private static void justMove(Player player, int number) {
         Number n1 = Board.getBoard().get(number - 1);
         player.setCurrentPosition(n1);
     }
 
-    private boolean checkSnake(Player p1) {
+    private static boolean checkSnake(Player p1) {
         return p1.getCurrentPosition().isSnake();
     }
 
-    private boolean checkLadder(Player p1) {
+    private static boolean checkLadder(Player p1) {
         return p1.getCurrentPosition().isLadder();
     }
 
