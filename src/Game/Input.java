8ff0b951d8e49a2dbe2fdb23aaf43c763c2d7033
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-06-03 18:12:47 +0530

    add a new method inputInt that ensure input of a Integer value

--- src/Game/Input.java
+++ src/Game/Input.java
@@ -1,4 +1,18 @@
 package Game;
 
+import java.util.Scanner;
+
 public class Input {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static int inputInt() {
+        while (true) {
+            String a = scanner.nextLine();
+            try {
+                return Integer.parseInt(a);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input");
+            }
+        }
+    }
 }
