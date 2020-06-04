f8b9c9bd03aecf25167d57fc4e31768fb8d63860
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-06-04 11:53:12 +0530

    call the starting method in the mainClass

--- src/Execution/Main.java
+++ src/Execution/Main.java
@@ -4,7 +4,6 @@ import Game.SnakeAndLadders;
 
 public class Main {
     public static void main(String[] args) {
-        SnakeAndLadders snakeAndLadders = new SnakeAndLadders();
-        snakeAndLadders.start();
+        SnakeAndLadders.startGame();
     }
 }
