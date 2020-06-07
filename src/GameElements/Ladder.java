09268dd505cbf65159820e876e2538d4ccf9387f
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-06-07 12:13:45 +0530

    rename head and tail field to top and down of the ladder class

--- src/GameElements/Ladder.java
+++ src/GameElements/Ladder.java
@@ -1,31 +1,32 @@
 package GameElements;
 
 public class Ladder {
-    private Number head;
-    private Number tail;
-    public void setHead(Number head) {
-        this.head = head;
+    private Number top;
+    private Number down;
+
+    public Ladder(Number top, Number down) {
+        this.top = top;
+        this.down = down;
     }
 
-    public void setTail(Number tail) {
-        this.tail = tail;
+    public Number getTop() {
+        return top;
     }
 
-    public Number getHead() {
-        return head;
+    public void setTop(Number top) {
+        this.top = top;
     }
 
-    public Ladder(Number head, Number tail) {
-        this.head = head;
-        this.tail = tail;
+    public Number getDown() {
+        return down;
     }
 
-    public Number getTail() {
-        return tail;
+    public void setDown(Number down) {
+        this.down = down;
     }
 
     public Player riseLadder(Player p1) {
-        p1.setCurrentPosition(this.getHead());
+        p1.setCurrentPosition(this.getTop());
         return p1;
     }
 
